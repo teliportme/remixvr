c6d4fc2a324b74e519f4b7b0a665c4b0f4f01c82
Rison Simon <rison@risonsimon.com>
2018-07-09 19:16:50 +0530

    added tests for backend

---
+++ backend/tests/conftest.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""Defines fixtures available to all tests."""
+
+import pytest
+from webtest import TestApp
+
+from remixvr.app import create_app
+from remixvr.database import db as _db
+from remixvr.settings import TestConfig
+from remixvr.profile.models import UserProfile
+
+
+from .factories import UserFactory
+
+
+@pytest.yield_fixture(scope='function')
+def app():
+    """An application for the tests."""
+    _app = create_app(TestConfig)
+
+    with _app.app_context():
+        _db.create_all()
+
+    ctx = _app.test_request_context()
+    ctx.push()
+
+    yield _app
+
+    ctx.pop()
+
+
+@pytest.fixture(scope='function')
+def testapp(app):
+    """A Webtest app."""
+    return TestApp(app)
+
+
+@pytest.yield_fixture(scope='function')
+def db(app):
+    """A database for the tests."""
+    _db.app = app
+    with app.app_context():
+        _db.create_all()
+
+    yield _db
+
+    # Explicitly close DB connection
+    _db.session.close()
+    _db.drop_all()
+
+
+@pytest.fixture
+def user(db):
+    """A user for the tests."""
+    class User():
+        def get(self):
+            muser = UserFactory(password='myprecious')
+            UserProfile(muser).save()
+            db.session.commit()
+            return muser
+    return User()
