4033e067168f0f248ea6b0350a56c1064d67feee
Rison Simon <rison@risonsimon.com>
2018-07-31 20:04:55 +0530

    updated starter template - moved aframe from html to npm dependency to make it easier to track - added html-webpack-plugin

--- packages/starter-template/webpack.config.js
+++ packages/starter-template/webpack.config.js
@@ -1,6 +1,10 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-let PLUGINS = [];
+let PLUGINS = [new HtmlWebpackPlugin({
+  template: 'index.html',
+  inject: 'head'
+})];
 if (process.env.NODE_ENV === 'production') {
   PLUGINS.push(new webpack.optimize.UglifyJsPlugin());
 }
