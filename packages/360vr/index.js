98ede235d63d697f79f3bf0c1cc059b283b8f4ed
Rison Simon <rison@risonsimon.com>
2018-08-02 19:24:48 +0530

    [WIP] Added 360VR to create 360 VR tours- hotspot text appears when hovering over the hotspot

---
+++ packages/360vr/index.js
@@ -0,0 +1,58 @@
+import AFRAME from 'aframe';
+import 'aframe-animation-component';
+import 'aframe-event-set-component';
+
+document.addEventListener('loaded', function() {
+  console.log('all items loaded')
+})
+
+AFRAME.registerComponent('set-image', {
+  schema: {
+    on: { type: 'string' },
+    target: { type: 'selector', default: '#space' },
+    src: { type: 'string' },
+    dur: { type: 'number', default: 300 }
+  },
+
+  init: function() {
+    var data = this.data;
+    var el = this.el;
+
+    this.setupFadeAnimation();
+
+    el.addEventListener(data.on, function() {
+      data.target.emit('set-image-fade');
+      setTimeout(function() {
+        data.target.setAttribute('material', 'src', data.src);
+      }, data.dur);
+    });
+  },
+
+  setupFadeAnimation: function() {
+    var data = this.data;
+    var targetEl = this.data.target;
+
+    if (targetEl.dataset.setImageFadeSetup) { return; }
+    targetEl.dataset.setImageFadeSetup = true;
+
+    targetEl.setAttribute('animation__fade', {
+      property: 'material.color',
+      startEvents: 'set-image-fade',
+      dir: 'alternate',
+      dur: data.dur + 700,
+      from: '#000',
+      to: '#fff'
+    })
+  }
+});
+
+AFRAME.registerComponent('face-camera', {
+
+  init: function() {
+    // wait for camera to be added to the scene
+    this.el.sceneEl.addEventListener('loaded', function() {
+      this.el.object3D.lookAt(this.el.sceneEl.camera.getWorldPosition());
+    }.bind(this))
+    
+  }
+});
