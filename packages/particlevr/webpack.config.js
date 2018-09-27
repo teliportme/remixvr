6e8a6ecc06100756a16a6c123e0d62e29485f3c7
Rison Simon <rison@risonsimon.com>
2018-09-27 14:37:57 +0530

    updated particlevr webpack config and added build output

--- packages/particlevr/webpack.config.js
+++ packages/particlevr/webpack.config.js
@@ -1,10 +1,14 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 let PLUGINS = [new HtmlWebpackPlugin({
   template: 'index.html',
   inject: 'head'
-})];
+}),
+  new CopyWebpackPlugin([
+    { from: 'images', to: 'images' }
+  ])];
 if (process.env.NODE_ENV === 'production') {
   PLUGINS.push(new webpack.optimize.UglifyJsPlugin());
 }
@@ -12,7 +16,7 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   entry: './index.js',
   output: {
-    path: __dirname,
+    path: __dirname + '/dist',
     filename: 'build.js'
   },
   module: {
