02eeaeb05d2535da167ab6584d70ca1f6ffe7784
Rison Simon <rison@risonsimon.com>
2019-03-15 19:00:01 +0530

    updated models and added serializers for field

--- backend/remixvr/field/models.py
+++ backend/remixvr/field/models.py
@@ -4,48 +4,34 @@ import datatime as dt
 
 from remixvr.database import Column, Model, SurrogatePK, db, reference_col
 
+
 class Field(SurrogatePK, Model):
 
     __tablename__ = 'field'
     label = Column(db.String(100))
-    field_type_id = reference_col('fieldtype', nullable=False)
-    field_type = relationship('FieldType', backref='fields')
+    type = Column(db.String(50))
     project_id = reference_col('project', nullable=False)
-    project = relationship('Project', backref='projects')
-    
+    project = relationship('Project', back_populates='fields')
+    parent_id = reference_col('field')
+    # https://docs.sqlalchemy.org/en/latest/_modules/examples/adjacency_list/adjacency_list.html
+    children = relationship(
+        "Field",
+        # cascade deletions
+        cascade="all, delete-orphan",
+        # many to one + adjacency list - remote_side
+        # is required to reference the 'remote'
+        # column in the join condition.
+        backref=db.backref("parent", remote_side=id),
+        # children will be represented as a dictionary
+        # on the "label" attribute.
+        collection_class=attribute_mapped_collection("label"),
+    )
 
-class FieldType(SurrogatePK, Model):
-
-    __tablename__ = 'fieldtype'
-    field_code = Column(db.String(5), unique=True, nullable=False)
-    field_name = Column(db.String(50), unique=True, nullable=False)
-
-
-class Position(Model):
-
-    __tablename__ = 'position'
-    id = db.Column(db.Integer, primary_key=True)
-    field_id = reference_col('field', nullable=False)
-    x = Column(db.Integer, default=0)
-    y = Column(db.Integer, default=0)
-    z = Column(db.Integer, default=0)
-    w = Column(db.Integer, default=1)
-
-
-class Text(Model):
-
-    __tablename__ = 'text'
-    id = Column(db.Integer, primary_key=True)
-    field_id = reference_col('field', nullable=False)
-    content = Column(db.String(200))
-
-
-class Number(Model):
-
-    __tablename__ = 'number'
-    id = db.Column(db.Integer, primary_key=True)
-    field_id = reference_col('field', nullable=False)
-    value = Column(db.Integer)
+    # https://docs.sqlalchemy.org/en/latest/orm/inheritance.html#joined-table-inheritance
+    __mapper_args__ = {
+        "polymorphic_identity": "field",
+        "polymorphic_on": type,
+    }
 
 
 class File(Model):
@@ -61,60 +47,88 @@ class File(Model):
                         default=dt.datetime.utcnow)
 
 
-class Audio(Model):
+class Position(Field):
+
+    __tablename__ = 'position'
+    id = db.Column(db.ForeignKey("field.id"), primary_key=True)
+    x = Column(db.Integer, default=0)
+    y = Column(db.Integer, default=0)
+    z = Column(db.Integer, default=0)
+    w = Column(db.Integer, default=1)
+
+    __mapper_args__ = {"polymorphic_identity": "position"}
+
+
+class Text(Field):
+
+    __tablename__ = 'text'
+    id = db.Column(db.ForeignKey("field.id"), primary_key=True)
+    value = Column(db.String(200))
+
+    __mapper_args__ = {"polymorphic_identity": "text"}
+
+
+class Number(Field):
+
+    __tablename__ = 'number'
+    id = db.Column(db.ForeignKey("field.id"), primary_key=True)
+    value = Column(db.Integer)
+
+    __mapper_args__ = {"polymorphic_identity": "number"}
+
+
+class Audio(Field):
 
     __tablename__ = 'audio'
-    id = db.Column(db.Integer, primary_key=True)
-    field_id = reference_col('field', nullable=False)
+    id = db.Column(db.ForeignKey("field.id"), primary_key=True)
     file_id = reference_col('file', nullable=False)
     duration = Column(db.Integer)
     audio_format = Column(db.String(50))
 
+    __mapper_args__ = {"polymorphic_identity": "audio"}
 
-class Video(Model):
+
+class Video(Field):
 
     __tablename__ = 'video'
-    id = db.Column(db.Integer, primary_key=True)
-    field_id = reference_col('field', nullable=False)
+    id = db.Column(db.ForeignKey("field.id"), primary_key=True)
     file_id = reference_col('file', nullable=False)
     duration = Column(db.Integer)
     width = Column(db.width)
     height = Column(db.height)
 
+    __mapper_args__ = {"polymorphic_identity": "video"}
 
-class VideoSphere(Model):
 
-    __tablename__ = 'video'
-    id = db.Column(db.Integer, primary_key=True)
-    field_id = reference_col('field', nullable=False)
+class VideoSphere(Field):
+
+    __tablename__ = 'videosphere'
+    id = db.Column(db.ForeignKey("field.id"), primary_key=True)
     file_id = reference_col('file', nullable=False)
     duration = Column(db.Integer)
     width = Column(db.width)
     height = Column(db.height)
 
+    __mapper_args__ = {"polymorphic_identity": "videosphere"}
 
-class Image(Model):
+
+class Image(Field):
 
     __tablename__ = 'image'
-    id = db.Column(db.Integer, primary_key=True)
-    field_id = reference_col('field', nullable=False)
+    id = db.Column(db.ForeignKey("field.id"), primary_key=True)
     file_id = reference_col('file', nullable=False)
     width = Column(db.width)
     height = Column(db.height)
 
+    __mapper_args__ = {"polymorphic_identity": "image"}
 
-class PhotoSphere(Model):
+
+class PhotoSphere(Field):
 
     __tablename__ = 'photosphere'
-    id = db.Column(db.Integer, primary_key=True)
-    field_id = reference_col('field', nullable=False)
+    id = db.Column(db.ForeignKey("field.id"), primary_key=True)
     file_id = reference_col('file', nullable=False)
     width = Column(db.width)
     height = Column(db.height)
 
-
-class FieldChild(Model):
-
-    __tablename__ = 'fieldchild'
-    field_id = reference_col('field', nullable=False)
-    child_id = reference_col('field', nullable=False)
+    __mapper_args__ = {"polymorphic_identity": "photosphere"}
---
+++ backend/remixvr/field/serializers.py
@@ -0,0 +1,98 @@
+from marshmallow import Schema, fields, pre_load, post_dump
+from marshmallow_oneofschema import OneOfSchema
+
+from remixvr.project.serializers import ProjectSchema
+from .models import (Position, Text, Number, Audio, Video,
+                     VideoSphere, Image, PhotoSphere)
+
+
+class FieldSchema(Schema):
+    label = fields.Str()
+    type = fields.Str()
+    project = fields.Nested(ProjectSchema)
+    children = fields.Nested('self', exclude=('children', ), default=None)
+
+   class Meta:
+        strict = True
+
+class FileSchema(Schema):
+    filename = fields.Str()
+    uri = fields.Str()
+    filemime = fields.Str()
+    filesize = fields.Int()
+    filename_original = fields.Str()
+    created_at = fields.DateTime()
+
+class PositionSchema(FieldSchema):
+    x = fields.Int()
+    y = fields.Int()
+    z = fields.Int()
+    w = fields.Int()
+
+class TextSchema(FieldSchema):
+    value = fields.Str()
+
+class NumberSchema(FieldSchema):
+    value = fields.Int()
+
+class AudioSchema(FieldSchema):
+    file = fields.Nested(FileSchema)
+    duration = fields.Int()
+    audio_format = fields.Str()
+
+class VideoSchema(FieldSchema):
+    file = fields.Nested(FileSchema)
+    duration = fields.Int()
+    width = fields.Int()
+    height = fields.Int()
+
+class VideoSphereSchema(FieldSchema):
+    file = fields.Nested(FileSchema)
+    duration = fields.Int()
+    width = fields.Int()
+    height = fields.Int()
+
+class ImageSchema(FieldSchema):
+    file = fields.Nested(FileSchema)
+    width = fields.Int()
+    height = fields.Int()
+
+class PhotoSphereSchema(FieldSchema):
+    file = fields.Nested(FileSchema)
+    width = fields.Int()
+    height = fields.Int()
+
+class ProjectFieldSchema(OneOfSchema):
+    type_schemas = {
+        'position': PositionSchema,
+        'text': TextSchema,
+        'number': NumberSchema,
+        'audio': AudioSchema,
+        'video': VideoSchema,
+        'videosphere': VideoSphereSchema,
+        'image': ImageSchema,
+        'photosphere': PhotoSphereSchema
+    }
+
+    def get_obj_type(self, obj):
+        if isinstance(obj, Position):
+            return 'position'
+        elif isinstance(obj, Text):
+            return 'text'
+        elif isinstance(obj, Number):
+            return 'number'
+        elif isinstance(obj, Audio):
+            return 'audio'
+        elif isinstance(obj, Video):
+            return 'video'
+        elif isinstance(obj, VideoSphere):
+            return 'videosphere'
+        elif isinstance(obj, Image):
+            return 'image'
+        elif isinstance(obj, PhotoSphere):
+            return 'photosphere'
+        else:
+            raise Exception('Unknown object type: %s' %obj.__class__.__name__)
+
+    class Meta:
+        strict = True
