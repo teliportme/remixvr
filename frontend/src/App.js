d0a9ce1d0869572bb1c70bfb730dcb9d31814893
Rison Simon <rison@risonsimon.com>
2019-03-26 17:29:04 +0530

    updated, react, mobx, react-scripts, added react lazy and suspense

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './containers/Home';
 import { inject, observer } from 'mobx-react';
-import Header from './components/Header';
-// import './App.css';
+import LoadingSpinner from './components/LoadingSpinner';
+const Header = lazy(() => import('./components/Header'));
+
+const AsyncHeader = (props) => (
+  <React.Suspense fallback={<div></div>}>
+    <Header {...props} />
+  </React.Suspense>
+)
+
 
 const DefaultLayout = inject("commonStore")(observer(({ commonStore, ...rest }) => (
   <React.Fragment>
-    <Header />
-      <Route { ...rest} />
+    <AsyncHeader />
+    <Route {...rest} />
   </React.Fragment>
 )));
 
+// const App = inject("userStore", "commonStore")
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App w-100 center h-100 flex flex-column">
-          <Switch>
-            <DefaultLayout path="/" component={Home} />
-          </Switch>
-        </div>
+        <Suspense fallback={<LoadingSpinner />}>
+          <div className="App w-100 center h-100 flex flex-column">
+            <Switch>
+              <DefaultLayout path="/" component={Home} />
+            </Switch>
+          </div>
+        </Suspense>
       </BrowserRouter>
     );
   }
