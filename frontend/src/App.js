bdae77e61d2bff0e8b19543ee93a7a27405a3dc4
Rison Simon <rison@risonsimon.com>
2019-03-26 18:55:10 +0530

    added prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,20 @@ import { inject, observer } from 'mobx-react';
 import LoadingSpinner from './components/LoadingSpinner';
 const Header = lazy(() => import('./components/Header'));
 
-const AsyncHeader = (props) => (
-  <React.Suspense fallback={<div></div>}>
+const AsyncHeader = props => (
+  <React.Suspense fallback={<div />}>
     <Header {...props} />
   </React.Suspense>
-)
+);
 
-
-const DefaultLayout = inject("commonStore")(observer(({ commonStore, ...rest }) => (
-  <React.Fragment>
-    <AsyncHeader />
-    <Route {...rest} />
-  </React.Fragment>
-)));
+const DefaultLayout = inject('commonStore')(
+  observer(({ commonStore, ...rest }) => (
+    <React.Fragment>
+      <AsyncHeader />
+      <Route {...rest} />
+    </React.Fragment>
+  ))
+);
 
 // const App = inject("userStore", "commonStore")
 class App extends Component {
