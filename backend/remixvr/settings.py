f9d85a97b1a86d4dc4aef5b58824043eb0a90612
Rison Simon <rison@risonsimon.com>
2019-03-27 14:20:27 +0530

    added localhost to cors whitelist

--- backend/remixvr/settings.py
+++ backend/remixvr/settings.py
@@ -18,6 +18,8 @@ class Config(object):
     JWT_AUTH_USERNAME_KEY = 'email'
     JWT_AUTH_HEADER_PREFIX = 'Token'
     CORS_ORIGIN_WHITELIST = [
+        'http://0.0.0.0:3000',
+        'http://localhost:3000',
         'http://0.0.0.0:4100',
         'http://localhost:4100',
         'http://0.0.0.0:8000',
