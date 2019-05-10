20ae3cde2a7cc11ded43bc59cf796c0e4257cd61
Rison Simon <rison@risonsimon.com>
2019-05-10 16:34:53 +0530

    updated dashboard to have a list of projects and button to create new project

--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -1,8 +1,10 @@
 import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import ProjectStore from '../stores/projectStore';
 import UserStore from '../stores/userStore';
 import { Helmet } from 'react-helmet';
 import { observer } from 'mobx-react-lite';
+import dayjs from 'dayjs';
 
 const Dashboard = observer(() => {
   const projectStore = useContext(ProjectStore);
@@ -18,12 +20,32 @@ const Dashboard = observer(() => {
   }, [projectStore]);
 
   return (
-    <div>
+    <div className="w-80-ns w-100 pa3 center">
       <Helmet title="Dashboard" />
-      {projectStore.count > 0 &&
-        projectStore.projects.map(project => (
-          <div key={project.slug}>{project.title}</div>
-        ))}
+      <div>
+        <Link
+          to="/create"
+          className="f6 link dim br2 ph3 pv2 mb2 dib white bg-blue"
+        >
+          Create project
+        </Link>
+      </div>
+      <ul className="list pl0 ml0 center mw6 ba b--light-silver br2">
+        {projectStore.count > 0 &&
+          projectStore.projects.map(project => (
+            <li key={project.slug} className="bb b--light-silver">
+              <Link
+                to={`/project/${project.slug}/edit`}
+                className="blue db f4 fw5 link ph3 pt3"
+              >
+                {project.title}
+              </Link>
+              <span className="db f6 gray i pb3 ph3 pt1 truncate">
+                {dayjs(project.created_at).format('MMM D, YYYY')}
+              </span>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 });
