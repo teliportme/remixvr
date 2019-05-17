a6deac74481e2249c50d031e7527b911cd9823e8
Rison Simon <rison@risonsimon.com>
2019-05-17 16:18:10 +0530

    updated dashboard ui

--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -20,30 +20,39 @@ const Dashboard = observer(() => {
   return (
     <div className="w-80-ns w-100 pa3 center">
       <Helmet title="Dashboard" />
+      <h2 className="fw7 f2">My Projects</h2>
       <div>
         <Link
           to="/create"
-          className="f6 link dim br2 ph3 pv2 mb2 dib white bg-blue"
+          className="f5 link dim br2 ph3 pv2 mb2 dib white bg-blue bb bw2 b--dark-blue"
         >
           Create project
         </Link>
       </div>
-      <ul className="list pl0 ml0 center mw6 ba b--light-silver br2">
-        {projectStore.count > 0 &&
-          projectStore.projects.map(project => (
-            <li key={project.slug} className="bb b--light-silver">
+      {projectStore.count > 0 ? (
+        <ul className="list pl0 ml0 mw6 bn">
+          {projectStore.projects.map(project => (
+            <li key={project.slug} className="bt pt3 b--light-green">
+              <button class="b--light-green bg-washed-green br-pill f6 fw7 pv1 tc ttc">
+                {`${project.theme.title} Theme`}
+              </button>
               <Link
                 to={`/project/${project.slug}/edit`}
-                className="blue db f4 fw5 link ph3 pt3"
+                className="db f3 fw7 link near-black pt2"
               >
                 {project.title}
               </Link>
-              <span className="db f6 gray i pb3 ph3 pt1 truncate">
+              <span className="db f6 gray pv2 truncate">
                 {dayjs(project.created_at).format('MMM D, YYYY')}
               </span>
             </li>
           ))}
-      </ul>
+        </ul>
+      ) : (
+        <div className="lh-copy mt3 dark-gray">
+          You haven't created any projects yet!
+        </div>
+      )}
     </div>
   );
 });
