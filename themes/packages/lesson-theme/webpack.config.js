f3930dedee019777e400c41de10bb669922b4634
Rison Simon <rison@risonsimon.com>
2019-05-23 13:01:50 +0530

    added async to build.js in lesson theme

--- themes/packages/lesson-theme/webpack.config.js
+++ themes/packages/lesson-theme/webpack.config.js
@@ -6,10 +6,10 @@ let PLUGINS = [
   new HtmlWebpackPlugin({
     template: 'index.html',
     inject: 'head'
+  }),
+  new ScriptExtHtmlWebpackPlugin({
+    defaultAttribute: 'async'
   })
-  // new ScriptExtHtmlWebpackPlugin({
-  //   defaultAttribute: 'async'
-  // })
 ];
 if (process.env.NODE_ENV === 'production') {
   PLUGINS.push(new webpack.optimize.UglifyJsPlugin());
