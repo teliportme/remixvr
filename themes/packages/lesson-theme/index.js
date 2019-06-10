ce63d45cb4c5fe061ee328a1542a28b946dcdd50
Rison Simon <rison@risonsimon.com>
2019-06-10 13:33:08 +0530

    fix2 for when sound field is empty in lesson theme

--- themes/packages/lesson-theme/index.js
+++ themes/packages/lesson-theme/index.js
@@ -70,7 +70,7 @@ function setupSpace() {
     const spaceType = space.type;
     const fields = space.fields;
     const soundField = getValues(fields, 'type', 'audio');
-    if (soundField && soundField.length > 0) {
+    if (soundField && soundField.file) {
       soundElement.setAttribute(
         'sound',
         'src',
