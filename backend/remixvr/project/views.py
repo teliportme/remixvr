1766355e8888197bc97412cac864c97679d2b1f9
Rison Simon <rison@risonsimon.com>
2019-06-27 18:41:09 +0530

    added remix project endpoint

--- backend/remixvr/project/views.py
+++ backend/remixvr/project/views.py
@@ -2,14 +2,18 @@
 
 import datetime as dt
 import json
+import uuid
 
 from flask import Blueprint
 from flask_apispec import use_kwargs, marshal_with
 from flask_jwt_extended import current_user, jwt_required, jwt_optional
 from marshmallow import fields
+from sqlalchemy.orm.session import make_transient
+from slugify import slugify
 
 from remixvr.user.models import User
 from remixvr.field.serializers import field_schemas
+from remixvr.field.models import Field
 from remixvr.space.serializers import space_schemas
 from remixvr.space.models import Space
 from remixvr.field.models import PhotoSphere, Image, Text
@@ -19,6 +23,7 @@ from .models import Project, Tags
 from .serializers import project_schema, projects_schema
 from remixvr.theme.serializers import theme_schema
 from remixvr.field.utils import generate_fields
+from remixvr.database import db
 
 blueprint = Blueprint('projects', __name__)
 
@@ -42,6 +47,57 @@ def get_projects(tag=None, author=None, favorited=None, limit=20, offset=0):
     if favorited:
         res = res.join(Project.favoriters).filter(User.username == favorited)
     return res.order_by(Project.created_at.desc()).offset(offset).limit(limit).all()
+
+
+@blueprint.route('/api/projects/remix', methods=('POST',))
+@jwt_required
+@use_kwargs(project_schema)
+@marshal_with(project_schema)
+def remix_project(slug, title, description, **kwargs):
+    project = Project.query.filter_by(slug=slug).first()
+    spaces = project.spaces
+    if not project:
+        raise InvalidUsage.project_not_found()
+    db.session.expunge(project)
+    make_transient(project)
+    project.author_id = current_user.profile.id
+    project.author = current_user.profile
+    project.title = title
+    project.id = None
+    project.created_at = None
+    project.updated_at = None
+    project.slug = slugify(title + "-" + str(uuid.uuid4().hex[:6].upper()))
+    project.description = description
+
+    for space_item in spaces:
+        space = Space.query.filter_by(id=space_item.id).first()
+        fields = space.fields
+        db.session.expunge(space)
+        make_transient(space)
+        space.id = None
+        space.author = current_user.profile
+        db.session.add(space)
+
+        for field_item in fields:
+            field = Field.query.filter_by(id=field_item.id).first()
+            # accessing the relations seems to keep item in the new field
+            if field.file:
+                file = field.file
+            db.session.expunge(field)
+            make_transient(field)
+            field.id = None
+            # field.space = space
+            field.space_id = space.id
+            field.author_id = current_user.profile.id
+            field.author = current_user.profile
+            db.session.add(field)
+            db.session.commit()
+
+        project.add_space(space)
+
+    db.session.add(project)
+    db.session.commit()
+    return project
 
 
 @blueprint.route('/api/projects', methods=('POST',))
