70d766161c4a89fe9f6e8417eaca6210818b1279
Rison Simon <rison@risonsimon.com>
2019-10-21 15:34:47 +0530

    added share options to lesson

---
+++ frontend/src/components/ProjectShareModal.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactModal from 'react-modal';
+import { FaUserLock } from 'react-icons/fa';
+import { FaUserFriends } from 'react-icons/fa';
+import { FaSchool } from 'react-icons/fa';
+
+const ProjectShareModal = ({ showModal, closeModal, projectCode }) => (
+  <ReactModal
+    appElement={document.body}
+    isOpen={showModal}
+    className="absolute shadow-2 bg-white br2 outline-0 pa2 top-2 bottom-1 left-1 right-1 measure-ns center"
+    shouldCloseOnOverlayClick={true}
+    shouldCloseOnEsc={true}
+    onRequestClose={closeModal}
+  >
+    <div className="tc relative">
+      <span className="b b--dashed b--gray ba br2 code dib f2 mt3 pa2 tracked">
+        {projectCode}
+      </span>
+      <span className="dark-gray db f6 mt1">
+        Share this code to access this lesson
+      </span>
+    </div>
+    <h2 className="dark-gray tc mt4">Lesson Sharing</h2>
+    <div className="flex justify-center">
+      <button className="b--light-blue bg-lightest-blue br2 flex flex-column items-center justify-around mh2 pointer outline-0 dim">
+        <FaUserLock className="h2 w2 blue pt2" />
+        <span className="f4 pb2">Private</span>
+      </button>
+      <button className="b--light-blue br2 flex flex-column items-center justify-around mh2 pointer outline-0 dim">
+        <FaUserFriends className="h2 w2 blue pt2" />
+        <span className="f4 pb2">Public</span>
+      </button>
+      <button className="b--light-blue br2 flex flex-column items-center justify-around mh2 pointer outline-0 dim">
+        <FaSchool className="h2 w2 blue pt2" />
+        <span className="f4 pb2">School</span>
+      </button>
+    </div>
+    <div className="absolute bottom-1 tc w-100">
+      <button
+        className="b--dark-gray ba bg-gray bl-0 br-0 br3 bt-0 bw2 dib dim f6 link mt3 ph3 pv2 white pointer"
+        onClick={closeModal}
+      >
+        close
+      </button>
+    </div>
+  </ReactModal>
+);
+
+export default ProjectShareModal;
--- frontend/src/containers/ProjectEdit.js
+++ frontend/src/containers/ProjectEdit.js
@@ -5,6 +5,7 @@ import SpacesCarousel from '../components/SpacesCarousel';
 import FieldsGenerate from '../components/FieldsGenerate';
 import ProjectStore from '../stores/projectStore';
 import SpaceList from '../components/SpaceList';
+import ProjectShareModal from '../components/ProjectShareModal';
 
 const ProjectEdit = observer(props => {
   const projectStore = useContext(ProjectStore);
@@ -12,6 +13,7 @@ const ProjectEdit = observer(props => {
   const [ready, setReady] = useState(false);
   const [currentSpace, setCurrentSpace] = useState(0);
   const [thisProject, setThisProject] = useState(null);
+  const [showShareModal, setShareModal] = useState(false);
 
   const projectSlug = props.match.params.slug;
   useEffect(() => {
@@ -77,13 +79,34 @@ const ProjectEdit = observer(props => {
               />
             </div>
             <div className="w-100 w-80-ns h-100 center ph3 ph0-ns fl overflow-auto measure-m">
+              <div className="ml3-ns mv2">
+                <button className="b--dark-green ba bg-green bl-0 br-0 br3 bt-0 bw2 dib dim f6 link mr3 mt3 ph3 pv2 white pointer">
+                  Save
+                </button>
+                <a
+                  className="b--dark-blue ba bg-blue bl-0 br-0 br3 bt-0 bw2 dib dim f6 link mt3 ph3 pv2 white mr3"
+                  target="_blank"
+                  href={`/lesson/${props.match.params.slug}/view`}
+                  rel="noopener noreferrer"
+                >
+                  View
+                </a>
+                <button
+                  onClick={() => {
+                    setShareModal(true);
+                  }}
+                  className="b--dark-red ba bg-red bl-0 br-0 br3 bt-0 bw2 dib dim f6 link mr3 mt3 ph3 pv2 white pointer"
+                >
+                  Share
+                </button>
+              </div>
               <div className="ml3-ns">
-                {thisProject && (
+                {/* {thisProject && (
                   <button className="b--light-green bg-washed-green br-pill f5 pv1 tc mv2 ph2">
                     Lesson Code:{' '}
                     <span className="fw7">{`${thisProject.code}`}</span>
                   </button>
-                )}
+                )} */}
                 <h2 className="fw7 f2 mv0">Enter lesson fields</h2>
                 <p className="f5 gray lh-copy">
                   Fill the value for each field. These values will be used for
@@ -95,21 +118,17 @@ const ProjectEdit = observer(props => {
                     spaceId={projectStore.spaces[currentSpace].id}
                   />
                 )}
-                <button className="b--dark-green ba bg-green bl-0 br-0 br3 bt-0 bw2 dib dim f6 link mr3 mt3 ph3 pv2 white pointer">
-                  Save Lesson
-                </button>
-                <a
-                  className="b--dark-blue ba bg-blue bl-0 br-0 br3 bt-0 bw2 dib dim f6 link mt3 ph3 pv2 white"
-                  target="_blank"
-                  href={`/lesson/${props.match.params.slug}/view`}
-                  rel="noopener noreferrer"
-                >
-                  View Lesson
-                </a>
               </div>
             </div>
           </div>
         ))}
+      <ProjectShareModal
+        showModal={showShareModal}
+        closeModal={() => {
+          setShareModal(false);
+        }}
+        projectCode={thisProject && thisProject.code}
+      />
     </React.Fragment>
   );
 });
